e4baea5e934534e865908a5f06045308be960b6f
SanOrbit <244919566+sanorbits@users.noreply.github.com>
2026-01-03 22:40:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src/assets'),
     },
   },
 })
