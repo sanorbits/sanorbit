68609a16e591b4ee0ba2d5892bd69998f7221d7d
SanOrbit <244919566+sanorbits@users.noreply.github.com>
2026-01-03 22:18:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/<sanorbit>/',
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
@@ -17,10 +18,4 @@ export default defineConfig({
     },
   },
 })
-export default defineConfig({
-  base: '/<sanorbit>/',
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
-  },
-})
+
